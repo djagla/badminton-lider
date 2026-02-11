a6b607275313ca08622c2102bec26c72a0cfa0da
djagla <46421379+djagla@users.noreply.github.com>
2026-02-11 13:39:42 +0100

    Add Google Fonts to WordPress theme

--- functions.php
+++ functions.php
@@ -1,5 +1,13 @@
 <?php
 
+function wpb_add_google_fonts() {
+  
+wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css2?family=Fugaz+One&display=swap', false ); 
+}
+  
+add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
+
+
 function myblocks_init() {
         register_block_type( __DIR__ . '/build/myheader' );
         register_block_type( __DIR__ . '/build/myfooter' );
