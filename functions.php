9913c66f90255bc57d0509f88cb059d89ae470e9
djagla <jagla.daniel@gmail.com>
2026-02-14 18:48:26 +0100

    Add Logo + tailwind

--- functions.php
+++ functions.php
@@ -1,8 +1,7 @@
 <?php
 
 function wpb_add_google_fonts() {
-  
-wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css2?family=Fugaz+One&display=swap', false ); 
+        wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css2?family=Fugaz+One&display=swap', false ); 
 }
   
 add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
@@ -12,5 +11,12 @@ function myblocks_init() {
         register_block_type( __DIR__ . '/build/myheader' );
         register_block_type( __DIR__ . '/build/myfooter' );
 }
+
 add_action( 'init', 'myblocks_init' );
 
+
+function add_tailwind() {
+        wp_enqueue_style('tailwind_output', get_template_directory_uri() . '/src/output.css'); 
+}
+  
+add_action('wp_enqueue_scripts', 'add_tailwind');
--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
-<?php wp_head(); ?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
+	<?php wp_head(); ?>
 </head>
 <body>
+	<img class="bl-logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/BL_logo.svg" alt="Badminton Lider" />
 
     <section class="hero">
 	<video class="hero-video" autoplay muted loop playsinline>
@@ -27,39 +28,39 @@
             </div>
         </div>
 	</div>
-</section>
-	
-<div style="background-color: #fff; overflow: hidden;">
-	<h1><?php bloginfo( 'name' ); ?></h1>
-	<h2><?php bloginfo( 'description' ); ?></h2>
-	
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	
-	<h3><?php the_title(); ?></h3>
-	
-	<?php the_content(); ?>
-	<?php wp_link_pages(); ?>
-	<?php edit_post_link(); ?>
-	
-	<?php endwhile; ?>
-	
-	<?php
-	if ( get_next_posts_link() ) {
-	next_posts_link();
-	}
-	?>
-	<?php
-	if ( get_previous_posts_link() ) {
-	previous_posts_link();
-	}
-	?>
-	
-	<?php else: ?>
-	
-	<p>No posts found. :(</p>
-	
-	<?php endif; ?>
-</div>
+	</section>
+		
+	<div style="background-color: #fff; overflow: hidden;">
+		<h1 class="bg-red-500"><?php bloginfo( 'name' ); ?></h1>
+		<h2><?php bloginfo( 'description' ); ?></h2>
+		
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		
+		<h3><?php the_title(); ?></h3>
+		
+		<?php the_content(); ?>
+		<?php wp_link_pages(); ?>
+		<?php edit_post_link(); ?>
+		
+		<?php endwhile; ?>
+		
+		<?php
+		if ( get_next_posts_link() ) {
+		next_posts_link();
+		}
+		?>
+		<?php
+		if ( get_previous_posts_link() ) {
+		previous_posts_link();
+		}
+		?>
+		
+		<?php else: ?>
+		
+		<p>No posts found. :(</p>
+		
+		<?php endif; ?>
+	</div>
 	
 <?php wp_footer(); ?>
 </body>
