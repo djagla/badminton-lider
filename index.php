dc0217c926a0bcce13ca0c0e1fcd9c5614298beb
djagla <jagla.daniel@gmail.com>
2026-02-19 21:16:58 +0100

    Header development

--- index.php
+++ index.php
@@ -10,95 +10,92 @@
 	<header>
 		<nav class="main-nav">
 			<a href="<?php echo get_home_url(); ?>">
-				<img class="bl-logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/BL_logo.svg" alt="Badminton Lider" />
+				<img class="bl-logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/bl_logo.svg" alt="Badminton Lider" />
 			</a>
 			
-			<?php
-				wp_nav_menu( array(
-					'menu' => 'main_navigation',
-					'theme_location' => 'primary',
-					'menu_class'     => 'nav-menu',
-				) );
-			?>
+			<div class="main-nav-wrapper">
+				<?php
+					wp_nav_menu( array(
+						'menu' => 'main_navigation',
+						'theme_location' => 'primary',
+						'menu_class'     => 'nav-menu',
+					) );
+				?>
 
-			<a href="">Wielicki Cup</a>
+				<a class="button-link white" href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/wc_logo.svg" alt="Badminton Lider" />Wielicki Cup</a>
+			</div>
+
+			<a class="button-link" href="">Treningi</a>
 		</nav>
 	</header>
 	
-
     <section class="hero">
-	<video class="hero-video" autoplay muted loop playsinline>
-		<source
-			src="<?php echo get_template_directory_uri(); ?>/assets/video/hero_hd.mp4"
-			type="video/mp4"
-		/>
+		<video class="hero-video" autoplay muted loop playsinline>
+			<source
+				src="<?php echo get_template_directory_uri(); ?>/assets/video/hero_hd.mp4"
+				type="video/mp4"
+			/>
 
-		Your browser does not support the video tag.
-	</video>
+			Your browser does not support the video tag.
+		</video>
 
-	<div class="hero-overlay"></div>
+		<div class="hero-overlay"></div>
 
-	<div class="hero-content">
-		<div class="typewriter">
-            <div>
-                <h1>BADMINTON<span>BADMINTON</span></h1>
-            </div>
-        </div>
-	</div>
+		<div class="hero-content">
+			<div class="typewriter">
+				<div>
+					<h1>BADMINTON<span>BADMINTON</span></h1>
+				</div>
+			</div>
+		</div>
 	</section>
 		
-	<div style="background-color: #fff; overflow: hidden;">
-		<!-- <h1 class="bg-red-500"><?php bloginfo( 'name' ); ?></h1>
+	<section class="news">
+		<h2>Aktualności</h2>
 
-		<h2><?php bloginfo( 'description' ); ?></h2> -->
-		
-		<section class="news">
-			<h2>Aktualności</h2>
+		<div class="news-list">
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-			<div class="news-list">
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-					<div class="news-item">
-						<?php if ( has_post_thumbnail() ) : ?>
-							<div class="news-item-thumbnail">
-								<?php the_post_thumbnail( 'medium' ); ?>
-							</div>
-						<?php endif; ?>
-
-						<?php the_category(); ?>
-
-						<div class="news-item-content">
-							<p class="news-date"><?php the_date(); ?></p>
-
-							<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-				
-							<a class="read-more" href="<?php the_permalink(); ?>">
-								<img src="<?php echo get_template_directory_uri(); ?>/assets/images/right_arrow.svg" alt="Czytaj więcej" />
-							</a>
+				<div class="news-item">
+					<?php if ( has_post_thumbnail() ) : ?>
+						<div class="news-item-thumbnail">
+							<?php the_post_thumbnail( 'medium' ); ?>
 						</div>
-					</div>
+					<?php endif; ?>
 
-					<?php endwhile; ?>
-						
-					<?php
-						if ( get_next_posts_link() ) {
-						next_posts_link();
-						}
-					?>
-					<?php
-						if ( get_previous_posts_link() ) {
-						previous_posts_link();
-						}
-					?>
-						
-					<?php else: ?>
+					<?php the_category(); ?>
+
+					<div class="news-item-content">
+						<p class="news-date"><?php the_date(); ?></p>
+
+						<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+			
+						<a class="read-more" href="<?php the_permalink(); ?>">
+							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/right_arrow.svg" alt="Czytaj więcej" />
+						</a>
+					</div>
+				</div>
+
+				<?php endwhile; ?>
 					
-					<p>No posts found. :(</p>
+				<?php
+					if ( get_next_posts_link() ) {
+					next_posts_link();
+					}
+				?>
+				<?php
+					if ( get_previous_posts_link() ) {
+					previous_posts_link();
+					}
+				?>
+					
+				<?php else: ?>
 				
-				<?php endif; ?>
-			</div>
-		</section>
-	</div>
+				<p>No posts found. :(</p>
+			
+			<?php endif; ?>
+		</div>
+	</section>
 	
 <?php wp_footer(); ?>
 </body>
