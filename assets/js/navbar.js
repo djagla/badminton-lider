12877fd409357ce3e96cb957e28038bdb3006999
djagla <jagla.daniel@gmail.com>
2026-02-28 09:38:02 +0100

    Header animation. Add new section

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,15 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function () {
-// 	const navbar = document.getElementById("navbar");
-
-// 	window.addEventListener("scroll", function () {
-// 		if (window.scrollY > 50) {
-// 			navbar.classList.add("nav-scrolled");
-// 		} else {
-// 			navbar.classList.remove("nav-scrolled");
-// 		}
-// 	});
-// });
-
 //const ENABLE_HIDE_ON_SCROLL = themeOptions.hideOnScroll;
 const ENABLE_HIDE_ON_SCROLL = false; // Set to true to enable hide-on-scroll behavior, false to disable
 
