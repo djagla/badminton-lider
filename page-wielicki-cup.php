2e8b0c88eea3871c90686a743208be7d52154a36
djagla <jagla.daniel@gmail.com>
2026-03-12 21:07:54 +0100

    Wielicki cup template

---
+++ page-wielicki-cup.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html class="!scroll-smooth">
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<title><?php wp_title( '|', true, 'right' ); ?></title>
+		<link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
+		<?php wp_head(); ?>
+	</head>
+
+	<body>
+		<main>
+            <section class="counter-section">
+                <a id="navbar-logo" class="bl-logo" href="<?php echo get_home_url(); ?>">
+                    <svg style="height: 200px; width: auto;" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" width="655.30817" height="787.26343" viewBox="0 0 173.38362 208.29678" version="1.1" id="svg3932">
+                        <defs id="defs3926"/>
+                        <metadata id="metadata3929">
+                            <rdf:RDF>
+                            <cc:Work rdf:about="">
+                                <dc:format>image/svg+xml</dc:format>
+                                <dc:type rdf:resource="http://purl.org/dc/dcmitype/StillImage"/>
+                            </cc:Work>
+                            </rdf:RDF>
+                        </metadata>
+                        <g id="layer1" transform="translate(-131.71991,-809.73109)">
+                            <g id="g1322-0" transform="matrix(4.7459788,0,0,4.7459788,-996.41697,-299.45128)">
+                            <text xml:space="preserve" style="font-style:normal;font-weight:normal;font-size:8.24667px;line-height:1.25;font-family:sans-serif;fill:#ffffff;fill-opacity:1;stroke:none;stroke-width:0.0759601" x="237.05225" y="267.80811" id="text1173-8-2-0-4-0-8-2-9-4-8-1-0-6-0-6-8-1-2-4-1-3-1-8-8-7-2-4-5-8-8-1-4-3-8-4-2-9-8-9"><tspan id="tspan1171-5-0-6-2-0-5-6-3-0-7-53-3-1-5-7-4-2-4-3-5-4-02-2-8-2-8-0-8-0-2-4-1-0-5-1-7-6-4-4" x="237.05225" y="267.80811" style="font-style:italic;font-variant:normal;font-weight:bold;font-stretch:normal;font-family:Okta;-inkscape-font-specification:'Okta Bold Italic';fill:#ffffff;fill-opacity:1;stroke-width:0.0759601">WIELICKI</tspan></text>
+                            <text xml:space="preserve" style="font-style:normal;font-weight:normal;font-size:11.1066px;line-height:1.25;font-family:sans-serif;word-spacing:0px;fill:#ffdb00;fill-opacity:1;stroke:none;stroke-width:0.166874" x="243.96523" y="277.48798" id="text1173-8-2-0-4-0-8-1-2-5-5-7-3-7-2-4-8-1-0-1-9-1-5-5-4-6-4-1-16-2-6-4-4-2-1-0-2-8-9-0-8"><tspan id="tspan1171-5-0-6-2-0-5-5-6-0-9-8-76-8-5-3-0-8-97-2-8-9-5-2-0-0-8-0-2-2-1-6-3-8-6-2-5-1-4-4-8" x="243.96523" y="277.48798" style="font-style:italic;font-variant:normal;font-weight:bold;font-stretch:normal;font-size:11.1066px;font-family:Okta;-inkscape-font-specification:'Okta Bold Italic';fill:#ffdb00;fill-opacity:1;stroke-width:0.166874">CUP</tspan></text>
+                            <g id="g1161-2" transform="matrix(0.29895304,0,0,0.29895304,137.31356,186.56144)">
+                                <g id="g1320-3-0-6-5-3-4" transform="rotate(40.079265,1085.808,-3165.9168)" style="fill:#ffffff;fill-opacity:1">
+                                <path id="path919-34-0-3-5-5-1-9-1-5-4-5-8-4-85-7-6-1-5" style="fill:#ffffff;fill-opacity:1;stroke:none;stroke-width:0.210726px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" d="m 2743.2307,-143.33278 -10.5554,3.91759 -5.4555,9.49348 9.016,8.17314 11.2975,-0.0114 2.097,-11.45098 z m -2.9921,3.73982 -3.2938,5.02244 -5.8048,2.33887 2.8716,-5.37125 z m 2.3947,0.16692 4.3103,6.58306 -2.0184,7.55819 0.1297,0.19793 -0.1473,-0.13281 v 5.3e-4 l -8.5514,0.88108 -5.9506,-5.16248 7.5324,-2.00814 6.9696,6.28902 0.017,-0.0651 -1.7772,-2.71094 -3.298,-5.03174 z"/>
+                                <path id="path1547-6-6-3-6-3-3-7-6-1-9-2-0-2-9-9-70-1-4-9-5" style="fill:#ffffff;fill-opacity:1;stroke:none;stroke-width:0.378005px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" d="m 2699.9589,-160.4068 -13.7713,0.1633 1.5565,10.63811 35.6299,15.14016 2.8701,-1.19512 0.4372,-2.77363 c -7.9337,-7.36085 -17.949,-14.88457 -26.7224,-21.97282 z m -0.096,4.15013 25.0744,17.33129 -0.1509,1.75051 -30.6684,-18.17798 z m -5.5455,3.77755 30.1315,15.68501 -1.9647,0.84321 -27.883,-14.26376 z"/>
+                                <path id="path1547-4-97-24-8-5-75-0-9-7-8-1-1-1-2-7-2-79-4-6-2-8-1" style="fill:#ffffff;fill-opacity:1;stroke:none;stroke-width:0.378005px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" d="m 2719.0735,-188.59423 -3.5424,13.14442 18.8023,30.07122 3.7165,0.15624 1.1401,-2.82466 -9.7492,-37.70036 z m 5.3211,7.03006 2.8422,0.82579 10.4476,31.37416 -0.3252,2.25004 z m -2.173,1.63298 14.6937,33.01881 -2.816,-0.55819 -14.4008,-27.48692 z"/>
+                                <path id="path1547-4-7-0-83-7-7-06-7-8-3-3-2-8-4-6-3-8-79-11-9-21-7" style="fill:#ffffff;fill-opacity:1;stroke:none;stroke-width:0.378005px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" d="m 2698.5334,-179.22466 1.0997,13.95419 27.5905,24.03316 2.9871,-1.21331 1.2892,-2.79575 -19.6521,-30.82499 z m 8.1654,7.92458 4.2618,0.33177 18.6815,24.96784 -0.5694,1.84795 z m -1.7622,1.56477 23.6927,25.97797 -2.0316,0.57523 -22.2032,-22.11472 z"/>
+                                </g>
+                                <path style="fill:#ffdb00;fill-opacity:1;stroke:none;stroke-width:0.365802px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" d="m 410.2659,206.694 c 12.2305,-5.4099 21.5996,-24.741 10.2012,-19.4285 16.7976,-15.5621 8.7092,13.9223 -10.2012,19.4285 z" id="path1221-7-3-4-2-3-39-3-1-0-1"/>
+                                <path style="fill:#ffdb00;fill-opacity:1;stroke:none;stroke-width:0.365802px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" d="m 383.4917,206.694 c -12.2305,-5.4099 -21.5996,-24.741 -10.2012,-19.4285 -16.7976,-15.5621 -8.7092,13.9223 10.2012,19.4285 z" id="path1221-7-3-4-2-3-39-3-1-2-5-1"/>
+                                <path style="fill:#ffdb00;fill-opacity:1;stroke:none;stroke-width:0.365802px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" d="m 384.3016,235.2922 c 12.2463,5.374 32.8341,-0.7587 21.2214,-5.5845 22.82,1.8926 -4.3958,15.8227 -21.2214,5.5845 z" id="path1221-7-3-4-2-3-39-3-1-7-5-5"/>
+                            </g>
+                            </g>
+                        </g>
+                    </svg>
+                </a>
+                <h1>Do turnieju pozostało</h1>
+                <p>30 dni</p>
+            </section>
+		</main>
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
